872085a5b99ee8b334c8a95e4025c3bb1c1ecc07
abanoub-refaat <abanoubref3at18@gmail.com>
2025-07-31 13:13:22 +0300

    feat: add experience and projects data for portfolio

---
+++ data/experience.ts
@@ -0,0 +1,49 @@
+const experience = [
+  {
+    title: "Web Development Committee at TechTalent",
+    subtitle: "",
+    startDate: "11/2024",
+    endDate: "Peresent",
+    location: "remote",
+    describtion: "Contributed to frontend and backend development tasks.",
+    techStack: ["TypeScript", "Next.js", "Tailwind CSS", "GitHub"],
+  },
+  {
+    title: "Front-end Developer Intern at EFG Holdings",
+    subtitle: "",
+    startDate: "01/07/2025",
+    endDate: "21/07/2025",
+    location: "The Smart Village",
+    describtion:
+      "Participated in a 3-week job shadowing program within the Frontend Department, supervised by a senior instructor.",
+    techStack: [
+      "TypeScript",
+      "Angular 20",
+      "Next.js",
+      "Tailwind CSS",
+      "GitHub",
+    ],
+  },
+  {
+    title: "Member of The Front-end Committee at Minders",
+    subtitle: "",
+    startDate: "02/2024",
+    endDate: "09/2024",
+    location: "Hybird",
+    describtion: "",
+    techStack: ["HTML5", "CSS", "JavaScript", "GitHub"],
+  },
+  {
+    title: "The Web Development Challenger Track",
+    subtitle:
+      "egFWD - The Ministry of Communication and Information Technology's Scholarship",
+    startDate: "09/2022",
+    endDate: "10/2022",
+    location: "Online",
+    describtion:
+      "Developed a deeper understanding of web accessibility and performance optimization.",
+    techStack: ["HTML5", "CSS", "JavaScript", "GitHub"],
+  },
+];
+
+export default experience;
---
+++ data/projects.ts
@@ -0,0 +1,58 @@
+const projects = [
+  {
+    title:
+      "Cine Sphere - OTT Platform with Ocean of Movies/Shows 2025 (Project Link))",
+    subtitle: "A responsive multi-page movie streaming platform",
+    startDate: "07/2025",
+    endDate: "07/2025",
+    describe: "",
+    techStack: [
+      "TypeScript",
+      "Next.js",
+      "Tailwind CSS",
+      "Git",
+      "GitHub",
+      "SCRUM",
+    ],
+    link: "https://github.com/abanoub-refaat/cine-sphere-app",
+  },
+  {
+    title: "Homy - Student Housing Mobile Application",
+    subtitle:
+      "A React Native app allowing students to view and rent rooms listed by sellers.",
+    startDate: "02/2025",
+    endDate: "05/2025",
+    describe: "",
+    techStack: ["TypeScript", "React Native", "Firebase", "Git", "GitHub"],
+    link: "https://github.com/abanoub-refaat/CS303_StudentHousingApp",
+  },
+  {
+    title: "DealZone - E-commerce Website",
+    subtitle:
+      "An e-commerce website with product browsing, cart management, and user authentication.",
+    startDate: "12/2024",
+    endDate: "02/2025",
+    describe: "",
+    techStack: [
+      "React.js",
+      "JavaScript",
+      "Express.js",
+      "Node.js",
+      "Git",
+      "GitHub",
+    ],
+    link: "https://github.com/abanoub-refaat/CS309_EcommerceProject_Front-end",
+  },
+  {
+    title: "Wack-a-Rabbit Game",
+    subtitle:
+      '2D Java game where players "whack" rabbits popping out of random holes.',
+    startDate: "12/2024",
+    endDate: "02/2025",
+    describe: "",
+    techStack: ["Java", "OpenGL", "OOP", "JOGL "],
+    link: "https://github.com/abanoub-refaat/CS304_FinalProject",
+  },
+];
+
+export default projects;
