ad0c7ab4e3900c9b8af8ed4733e706be3726341b
abanoub-refaat <abanoubref3at18@gmail.com>
2025-08-04 13:11:51 +0300

    feat: update HeroSection content and improve text description for clarity

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,17 +10,20 @@ const HeroSection = () => {
           Hello, I&apos;m Abanoub Refaat A Front-end Develper
         </h1>
         <p className="w-full flex justify-center items-center  lg:w-[85%] xl:w-[75%] text-lg sm:text-xl md:text-2xl text-[#7b3b4bd3]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit optio
-          tempora natus laborum ut quos, repellat nemo exercitationem voluptate
-          quis. Accusantium ex est cupiditate et dolore voluptates iusto dolorem
-          debitis.
+          specialize in creating responsive, user-friendly web applications
+          using modern front-end technologies like React, JavaScript, and
+          Bootstrap. I have a solid foundation in Algorithms, Data Structures,
+          Database Systems, Object-Oriented Programming, and Design Patterns,
+          complemented by strong problem-solving skills. I am passionate about
+          mastering front-end frameworks, API integrations, and improving user
+          experience.
         </p>
         <ButtonSolidLight
           title="Contact Me"
           styles="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 uppercase self-center lg:self-start"
         />
       </div>
-      <div className="hero-img w-full lg:w-auto flex justify-center">
+      <div className="hero-img w-full lg:w-auto flex justify-center items-center">
         <img
           src="images/hero-section-img-skills.png"
           alt="Abanoub Refaat image"
