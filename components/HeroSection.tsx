2c8661ca375683ac2fb5c3baecf95f4e03a8381f
abanoub-refaat <abanoubref3at18@gmail.com>
2025-08-10 11:29:18 +0300

    feat: refine layout and styling in HeroSection, Navbar, and SkillCard components

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,8 +4,8 @@ import ButtonSolidLight from "@/components/UI/ButtonSolidLight";
 
 const HeroSection = () => {
   return (
-    <section className="flex flex-col-reverse text-center justify-center lg:text-start lg:flex-row py-4 sm:px-6 md:px-8 items-center bg-[#DFD4D8] gap-5 lg:h-screen lg:rounded-bl-3xl lg:rounded-br-3xl">
-      <div className="hero-text flex flex-col gap-4cpy-4 px-2 md:px-4 w-full lg:w-[46%]">
+    <section className="flex flex-col-reverse text-center gap-3 lg:gap-0 lg:text-start lg:flex-row justify-center overflow-hidden p-6 items-center bg-[#DFD4D8] lg:h-screen lg:rounded-bl-3xl lg:rounded-br-3xl">
+      <div className="hero-text flex flex-col gap-4 md:w-[50%]">
         <h1 className="font-extrabold uppercase text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl w-full lg:w-[95%] text-[#7B3B4b]">
           Hello, I&apos;m Abanoub Refaat A Frontend Develper
         </h1>
@@ -21,11 +21,11 @@ const HeroSection = () => {
           styles="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 uppercase self-center lg:self-start"
         />
       </div>
-      <div className="hero-img w-full lg:w-auto flex justify-center items-center">
+      <div className="hero-img max-h-screen flex">
         <img
           src="images/hero-section-img-skills.png"
           alt="Abanoub Refaat image"
-          className="w-full max-w-md lg:max-w-none h-auto object-contain"
+          className=""
         />
       </div>
     </section>
